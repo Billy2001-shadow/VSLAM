e6cff68a9137db8362ba6089fa7c1ba00e652a82
chenwu <1113227202@qq.com>
2023-12-05 16:00:10 +0800

    add ch13 code

---
+++ app/run_kitti_stereo.cpp
@@ -0,0 +1,19 @@
+//
+// Created by gaoxiang on 19-5-4.
+//
+
+#include "myslam/visual_odometry.h"
+#include <gflags/gflags.h>
+
+DEFINE_string(config_file, "/home/cw/Slam/VSLAM/config/default.yaml",
+              "config file path");
+
+int main(int argc, char **argv) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  myslam::VisualOdometry::Ptr vo(new myslam::VisualOdometry(FLAGS_config_file));
+  assert(vo->Init() == true);
+  vo->Run();
+
+  return 0;
+}
